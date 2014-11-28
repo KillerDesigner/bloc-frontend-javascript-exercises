d88c39f7187e50b8ea4a76f642a3a7d79fcc8cdd
KillerDesigner <git@killerdesigner.com>
2014-11-28 06:03:10 -1000

    Solve Loops challenges

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,17 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+  var total = 0;
+  for (var i = 0; i < array.length; ++i) {
+    total += array[i];  
+  }
+  return total;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+  return inputString.toLowerCase().split(',');
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(newArray) {
+  return newArray.map(function(dataElement, index) {
+    return index + ' is ' + dataElement;
+  })
+};
